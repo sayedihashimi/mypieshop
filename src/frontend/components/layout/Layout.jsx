75cd4415cb746c4f2570c3186ee0b82073efd508
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2026-02-05 10:43:30 -0500

    naide: styling updates

--- src/frontend/components/layout/Layout.jsx
+++ src/frontend/components/layout/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer';
 
 function Layout() {
   return (
-    <div className="min-h-screen flex flex-col bg-amber-50">
+    <div className="min-h-screen flex flex-col bg-gray-50">
       <Header />
       <main className="flex-grow container mx-auto px-4 py-8">
         <Outlet />
